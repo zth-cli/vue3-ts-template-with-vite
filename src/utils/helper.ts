e1aabd18bffe376379fbd80fe5adfb9713ea650b
rzx100 <770304867@qq.com>
2022-09-05 17:46:32 +0800

    方法

---
+++ src/utils/helper.ts
@@ -0,0 +1,22 @@
+import type { App, Plugin } from 'vue'
+import { isObject } from './is'
+// 注册组件
+export const withInstall = <T>(component: T, alias?: string) => {
+  const comp = component as any
+  comp.install = (app: App) => {
+    app.component(comp.name || comp.displayName, component)
+    if (alias) {
+      app.config.globalProperties[alias] = component
+    }
+  }
+  return component as T & Plugin
+}
+
+// 深度合并
+export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
+  let key: string
+  for (key in target) {
+    src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
+  }
+  return src
+}
