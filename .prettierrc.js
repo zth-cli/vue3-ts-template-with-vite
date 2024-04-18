ead4c979b7329ebb2806d790e1bffd327d029517
rzx007 <770304867@qq.com>
2024-04-18 23:15:07 +0800

    chore: 添加.vscode配置，统一编辑器开发环境

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  tabWidth: 2,
-  jsxSingleQuote: true,
-  jsxBracketSameLine: true,
-  printWidth: 120,
-  singleQuote: true,
-  semi: false,
+  tabWidth: 2, // Tab 用两个空格代替
+  jsxSingleQuote: true, // JSX 使用单引号
+  jsxBracketSameLine: true, // JSX 标签的反尖括号不需要换行
+  printWidth: 120, // 一行最多 120 字符
+  singleQuote: true, // 使用单引号
+  semi: false, // 不添加尾部分号
+  arrowParens: 'always',// 箭头函数，只有一个参数的时候，也需要括号
   endOfLine: 'auto',
   overrides: [
     {
@@ -14,5 +15,4 @@ module.exports = {
       },
     },
   ],
-  arrowParens: 'always',
 }
